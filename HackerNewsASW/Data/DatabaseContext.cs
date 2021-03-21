1953e2e0a36eb3440edc78f4e42a4ffb038e423d
Salt4f <47352634+Salt4f@users.noreply.github.com>
2021-03-21 16:51:30 +0100

    Added database context class

---
+++ HackerNewsASW/Data/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HackerNewsASW.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HackerNewsASW.Data
+{
+    public class ReportsContext : DbContext
+    {
+        public ReportsContext(DbContextOptions<ReportsContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Model> Models { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*modelBuilder.Entity<MultiplePrimaryKeyModel>()
+                .HasKey(m => new { m.ID_1, m.ID_2 });*/
+        }
+
+    }
+}
