460dfe2d4900058778a89b7cf290bf2bc9ad1370
Salt4f <47352634+Salt4f@users.noreply.github.com>
2021-03-25 18:26:35 +0100

    Automated DB creation

--- HackerNewsASW/Program.cs
+++ HackerNewsASW/Program.cs
@@ -13,7 +13,30 @@ namespace HackerNewsASW
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    //var logger = services.GetRequiredService<ILogger<Program>>();
+                    var context = services.GetRequiredService<DatabaseContext>();
+                    //string sql = context.Database.GenerateCreateScript();
+                    //logger.LogInformation(sql);
+
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+
+            //var sql = dbContext.Database.GenerateCreateScript();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
