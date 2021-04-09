27828b4563449379f3f3173d8c00957b8ed4b083
Salt4f <47352634+Salt4f@users.noreply.github.com>
2021-04-09 16:35:01 +0200

    Submit reescrito sin mirar los fallos

--- HackerNewsASW/Controllers/NewsController.cs
+++ HackerNewsASW/Controllers/NewsController.cs
@@ -52,22 +52,100 @@ namespace HackerNewsASW.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> Submit([Bind("Title, Content")] News news)
+        public async Task<IActionResult> Submit([Bind("Title, Url, Text")] SubmitFormModel submit)
         {
-            if ((news.Title != null && news.Title.Trim().Length != 0) &&
-                (news.Content != null && news.Content.Trim().Length != 0))
+            if (submit.Title != null)
             {
-                news.DateCreated = DateTime.Now;
-                news.Author = await _context.Users.FindAsync(GetUserEmail(User));
-                _context.Add(news);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                User author = await _context.Users.FindAsync(GetUserEmail(User));
+
+                if (submit.Url is null && submit.Text is null) //ASK
+                {
+                    Ask ask = new Ask
+                    {
+                        Title = submit.Title,
+                        Author = author,
+                        Content = "",
+                        DateCreated = DateTime.Now
+                    };
+
+                    await _context.AddAsync(ask);
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(New)); //Habría que redireccionar a la inspección de la contribución
+                }
+                else if (submit.Url is null) //ASK
+                {
+                    Ask ask = new Ask
+                    {
+                        Title = submit.Title,
+                        Author = author,
+                        Content = submit.Text,
+                        DateCreated = DateTime.Now
+                    };
+
+                    await _context.AddAsync(ask);
+
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(New)); //Habría que redireccionar a la inspección de la contribución
+                }
+                else //URL
+                {
+                    var url = _context.News.FirstOrDefaultAsync(n => n.Content == submit.Url);
+                    if (url != null)
+                    {
+                        //Aquí habría que redireccionar a la función que se encargue de visualizar una noticia y sus comentarios
+                        return View();
+                    }
+
+                    if (submit.Text is null) //URL
+                    {
+                        News news = new News
+                        {
+                            Title = submit.Title,
+                            Author = author,
+                            Content = submit.Url,
+                            DateCreated = DateTime.Now
+                        };
+
+                        await _context.AddAsync(news);
+
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(New)); //Habría que redireccionar a la inspección de la contribución
+                    }
+                    else //URL + COMMENT
+                    {
+                        News news = new News
+                        {
+                            Title = submit.Title,
+                            Author = author,
+                            Content = submit.Url,
+                            DateCreated = DateTime.Now,
+                            Comments = new HashSet<Comment>()
+                        };
+
+                        Comment com = new Comment
+                        {
+                            Author = author,
+                            Content = submit.Text,
+                            DateCreated = DateTime.Now,
+                            Commented = news
+                        };
+
+                        news.Comments.Add(com);
+
+                        await _context.AddAsync(news);
+                        await _context.AddAsync(com);
+
+                        await _context.SaveChangesAsync();
+                        return RedirectToAction(nameof(New)); //Habría que redireccionar a la inspección de la contribución
+                    }
+                }
             }
-            return View(news);
+            return View(submit);
         }
 
         // GET: Contribucions/Edit/5
-        public async Task<IActionResult> Edit(long? id)
+        /*public async Task<IActionResult> Edit(long? id)
         {
             if (id == null)
             {
@@ -80,14 +158,14 @@ namespace HackerNewsASW.Controllers
                 return NotFound();
             }
             return View(contribucio);
-        }
+        }*/
 
         // POST: Contribucions/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
+        //[HttpPost]
         //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Url,Points,Author,Date,Title,Id")] Contribution contribucio)
+        /*public async Task<IActionResult> Edit(long id, [Bind("Url,Points,Author,Date,Title,Id")] Contribution contribucio)
         {
             if (id != contribucio.Id)
             {
@@ -115,10 +193,10 @@ namespace HackerNewsASW.Controllers
                 return RedirectToAction(nameof(Index));
             }
             return View(contribucio);
-        }
+        }*/
 
         // GET: Contribucions/Delete/5
-        public async Task<IActionResult> Delete(long? id)
+        /*public async Task<IActionResult> Delete(long? id)
         {
             if (id == null)
             {
@@ -133,23 +211,18 @@ namespace HackerNewsASW.Controllers
             }
 
             return View(contribucio);
-        }
+        }*/
 
         // POST: Contribucions/Delete/5
-        [HttpPost, ActionName("Delete")]
+        //[HttpPost, ActionName("Delete")]
         //[ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(long id)
+        /*public async Task<IActionResult> DeleteConfirmed(long id)
         {
             var contribucio = await _context.Contributions.FindAsync(id);
             _context.Contributions.Remove(contribucio);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool ContribucioExists(long id)
-        {
-            return _context.Contributions.Any(e => e.Id == id);
-        }
+        }*/
 
         private static string GetUserID(System.Security.Claims.ClaimsPrincipal user)
         {
