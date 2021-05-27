7dddd287d9561c8cef93ffec436720b0319d901a
marc10010 <75836335+marc10010@users.noreply.github.com>
2021-05-27 23:17:02 +0200

    correccion api

--- HackerNewsAngular/src/app/services/api.service.ts
+++ HackerNewsAngular/src/app/services/api.service.ts
@@ -26,11 +26,11 @@ export class ApiService {
       headers.append('Content-Type', 'application/json');
       headers.append('x-api-key', 'f6e205fda57d593885b06558834952d4cd2149f584e6d8b265438e0edfcefe0ef8e15f8cf343797370e8686166ae6bf14efea275cc206dbde35826c0d637e176');
     }
-
+*/
     createAuthorizationHeader(headers: HttpHeaders) : HttpHeaders {
       return headers.append('x-api-key', environment.apikey)//.append('hola-hola', 'hola');
     }
-*/
+
     obtenerNewsByVote(){
       let header = new HttpHeaders();
       header = this.createAuthorizationHeader(header);      
